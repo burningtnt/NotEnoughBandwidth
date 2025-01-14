bb5d4e5e971d8e99158caf1d3b9a549eab0e0907
USS-Shenzhou <969840795@qq.com>
2025-01-14 20:57:14 +0800

    first commit

---
+++ src/main/java/cn/ussshenzhou/notenoughbandwidth/NotEnoughBandwidth.java
@@ -0,0 +1,24 @@
+package cn.ussshenzhou.notenoughbandwidth;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import org.slf4j.Logger;
+
+/**
+ * @author USS_Shenzhou
+ */
+@Mod(NotEnoughBandwidth.MODID)
+public class NotEnoughBandwidth {
+    public static final String MODID = "nebw";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public NotEnoughBandwidth(IEventBus modEventBus, ModContainer modContainer) {
+        NeoForge.EVENT_BUS.register(this);
+
+        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        //modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+}
